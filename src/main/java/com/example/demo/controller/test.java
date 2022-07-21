b83633140d6fbf8e1bd2e11eadecaef564d44a75
manh.dd <manh.dd@kss.com.vn>
2022-07-21 18:09:25 +0700

    update

--- src/main/java/com/example/demo/controller/test.java
+++ src/main/java/com/example/demo/controller/test.java
@@ -1,2 +1,15 @@
-package com.example.demo.controller;public class test {
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+
+    @GetMapping("/hello")
+    public ResponseEntity get() {
+        return ResponseEntity.ok("hello");
+    }
+
 }
